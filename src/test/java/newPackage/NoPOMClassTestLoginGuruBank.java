72db0e67fe9c5e18357121061ddf085de358f908
ceko1 <cccceeekko@yahoo.com>
2016-05-03 15:32:42 -0500

    firstCommitToMaster
    commitmetomaster

---
+++ src/test/java/newPackage/NoPOMClassTestLoginGuruBank.java
@@ -0,0 +1,26 @@
+package newPackage;        
+ 
+//import org.openqa.selenium.By;      
+import org.openqa.selenium.WebDriver;       
+import org.openqa.selenium.firefox.FirefoxDriver;       
+import org.testng.Assert;       
+import org.testng.annotations.Test; 
+import org.testng.annotations.BeforeTest;   
+import org.testng.annotations.AfterTest;        
+public class NoPOMClassTestLoginGuruBank {      
+        private WebDriver driver;       
+        @Test              
+        public void testEasy() {    
+            driver.get("http://www.guru99.com/selenium-tutorial.html");  
+            String title = driver.getTitle();                
+            Assert.assertTrue(title.contains("Free Selenium Tutorials"));       
+        }   
+        @BeforeTest
+        public void beforeTest() {  
+            driver = new FirefoxDriver();  
+        }       
+        @AfterTest
+        public void afterTest() {
+            driver.quit();          
+        }       
+}   
